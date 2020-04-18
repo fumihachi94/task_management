d8690f47cd94dd47b23cdc3406eaa41eac0206dd
Fumiya.Sato <fumiya.sato.rop@gmail.com>
2020-04-18 23:53:15 +0900

    mod: create gantt chart program. Display task list and its contensts in left panel.

--- test/gantt_chart.py
+++ test/gantt_chart.py
@@ -3,6 +3,7 @@ import calendar
 import wx
 import wx.lib.scrolledpanel as scrolled
 from dateutil.relativedelta import relativedelta
+from create_calendar import  CreateCalendar
 
 class GanttChart(wx.App):
    def __init__(self, width, height, title='ChanttChart', redirect=False):
@@ -11,20 +12,20 @@ class GanttChart(wx.App):
       self.frame = wx.Frame(None, -1, title, size=(width,height))
       self.InitMenu()
 
-      self.panel1 = scrolled.ScrolledPanel(self.frame, wx.ID_ANY, size=(500,400))
-      self.panel1.SetBackgroundColour('snow')
-      self.panel2 = scrolled.ScrolledPanel(self.frame, wx.ID_ANY)
-      self.panel2.SetupScrolling()
+      self.base_panel1 = scrolled.ScrolledPanel(self.frame, wx.ID_ANY, size=(500,400))
+      self.base_panel1.SetBackgroundColour('snow')
+      self.base_panel2 = scrolled.ScrolledPanel(self.frame, wx.ID_ANY)
+      self.base_panel2.SetupScrolling()
 
       # カレンダーの表示
-      self.CreateCalendar()
+      CreateCalendar(self.base_panel2)
 
       # Task一覧の表示
-      
+
 
       hbox  = wx.BoxSizer(wx.HORIZONTAL)
-      hbox.Add(self.panel1,  1, wx.ALL | wx.GROW, border=1)
-      hbox.Add(self.panel2, 20, wx.ALL | wx.GROW, border=1)
+      hbox.Add(self.base_panel1,  1, wx.ALL | wx.GROW, border=1)
+      hbox.Add(self.base_panel2, 20, wx.ALL | wx.GROW, border=1)
       self.frame.SetSizer(hbox)
 
       self.frame.Show()
@@ -53,41 +54,6 @@ class GanttChart(wx.App):
 
    def Quit(self, event):
       self.frame.Close()
-
-   def CreateCalendar(self):
-      disp_year = 3
-      base_date = datetime.date(2020, 4, 1)
-      calendar_size  = {'size_month': (120, 30), 'size_week': (30, 30)}
-
-      layout_calendar = wx.BoxSizer(wx.VERTICAL)
-      layout_month    = wx.BoxSizer(wx.HORIZONTAL)
-      layout_week     = wx.BoxSizer(wx.HORIZONTAL)
-
-      # Create monthly block
-      for i in range(12*disp_year):
-         date = base_date + relativedelta(months=i)
-         month_button = wx.Button(self.panel2, wx.ID_ANY, '`'+ str(date.year)[2:4] + '/' + str(date.month), size=calendar_size['size_month'])
-         layout_month.Add(month_button, 1, wx.ALL | wx.SHAPED, 0)
-         # Create weekly block
-         for w in range(4):
-            monday = self.GetDayofNthDow(date.year, date.month, w+1, 0)
-            week_button = wx.Button(self.panel2, wx.ID_ANY, str(monday), size=calendar_size['size_week'])
-            layout_week.Add(week_button, 1, wx.ALL | wx.SHAPED, 0)
-
-      layout_calendar.Add(layout_month)
-      layout_calendar.Add(layout_week)
-      self.panel2.SetSizer(layout_calendar)
-
-   def GetDayofNthDow(self, year, month, nth, dow):
-      # [Pythonで第何何曜日（第2月曜日など）の日付を取得 | note.nkmk.me](https://note.nkmk.me/python-calendar-day-of-nth-dow/)
-      '''dow: Monday(0) - Sunday(6)'''
-      if nth < 1 or dow < 0 or dow > 6:
-         return None
-      
-      first_dow, n = calendar.monthrange(year, month)
-      day = 7 * (nth - 1) + (dow - first_dow) % 7 + 1
-
-      return day if day <= n else None
 
 
 def main():
--- test/genGanttChart.py
+++ test/genGanttChart.py
@@ -1,240 +1,109 @@
-# https://pillow.readthedocs.io/en/3.1.x/reference/ImageDraw.html
-import datetime
-import sys
-import random
-from PIL import Image, ImageDraw, ImageFont
+import wx
+import wx.grid as gridlib
+import wx.lib.scrolledpanel as scrolled
+from wx.lib.splitter import MultiSplitterWindow
+import wx.lib.mixins.listctrl as listmix
+import create_calendar
 
-class GanttChart:
+class TaskList(wx.ListCtrl, listmix.TextEditMixin):
+    def __init__(self, parent, ID, pos=wx.DefaultPosition,
+                 size=wx.DefaultSize, style=wx.LC_REPORT):
+        wx.ListCtrl.__init__(self, parent, ID, pos, size, style)
 
-    num = 0
-    top_margin = 10
-    bottom_margin = 10
-    left_margin = 10
-    cell_width = 32
-    date_position = 20
-    week_position = 40
-    box_position = 60
-    box_margin = 5
-    box_height = 40
-    font_size = 12
-    bar_color = (128,128,128)
-    font_color = (0,0,0)
-    line_color = (0,0,0)
-    holiday_color = (220,220,220)
-    max_day = 14
+        self.Bind(wx.EVT_LIST_END_LABEL_EDIT, self.LsitItemActivated)
 
-    font_regular = 'NotoSansCJKjp-Light.otf'
-    font_bold = 'NotoSansCJKjp-Bold.otf'
+        listmix.TextEditMixin.__init__(self)
 
-    def __init__(self,size=(512,256),color=(255,255,255)):
-        self.im = Image.new("RGB",size,color) #(512, 256), (255, 255, 255)
-        self.draw = ImageDraw.Draw(self.im)
+        # カラム
+        self.InsertColumn(0, "#",           wx.LIST_FORMAT_CENTRE, wx.LIST_AUTOSIZE_USEHEADER)
+        self.InsertColumn(1, "Task",        wx.LIST_FORMAT_CENTRE, 150)
+        self.InsertColumn(2, "Assign",      wx.LIST_FORMAT_CENTRE, 100)
+        self.InsertColumn(3, "Priority",    wx.LIST_FORMAT_CENTRE, 100)
+        self.InsertColumn(4, "Status",      wx.LIST_FORMAT_CENTRE, 100)
+        self.InsertColumn(5, "Description", wx.LIST_FORMAT_CENTRE, 100)
 
-        self.im_width = size[0]
-        self.im_height = size[1]
-
-        self.bg_color = color
-
-        self.today = datetime.date.today()
-        self.monday = self.today - datetime.timedelta(days=self.today.weekday())
-
-        self.calendar_height = size[1] - self.top_margin - self.bottom_margin
-
-    def get_monday(self):
-        return self.monday
-
-    def set_box_position(self,box_position):
-        self.box_position = box_position
-
-    def set_max_day(self,max_day):
-        self.max_day = max_day
-
-    def set_bar_height(self,height):
-        self.box_height = height
-
-    def set_bar_width(self,width):
-        self.cell_width = width
+        # ListCtrlにアイテムを追加
+        for x in range(40):
+            self.InsertItem(x, '%s' % x)
+            self.SetItem(x, 1, 'Task%s' % x)
+            self.SetItem(x, 2, 'Member%s' % x)
+            self.SetItem(x, 3, str((x + 1) * 5))
+            self.SetItem(x, 4, str((x + 3) * 5))
+            self.SetItem(x, 5, str((x + 5) * 5))
     
-    def set_bar_color(self,color):
-        self.bar_color = color
-
-    def set_line_color(self,color):
-        self.line_color = color
-
-    def set_holiday_color(self,color):
-        self.holiday_color = color
-
-    def set_font_color(self,color):
-        self.font_color = color
-
-    def set_font_size(self,size):
-        self.font_size = size
-
-    def set_left_margin(self,margin):
-        self.left_margin = margin
-
-    def set_top_margin(self,margin):
-        self.top_margin = margin
-
-    def set_week_position(self,position):
-        self.week_position = position
-
-    def set_date_position(self,position):
-        self.date_position = position
-
-    def draw_campain(self,start,end,title):
-
-        start_date = datetime.datetime.strptime(start,'%Y-%m-%d').date()
-        end_date = datetime.datetime.strptime(end,'%Y-%m-%d').date()
-
-        start_pos = (start_date - self.monday).days
-        if start_pos < 0:
-            start_pos = 0
-        if start_pos > self.max_day:
-            start_pos = self.max_day
-        end_pos = (end_date - self.monday).days
-        if end_pos > self.max_day:
-            end_pos = self.max_day -1
-
-        self.draw.rectangle(
-            [
-                (
-                    start_pos * self.cell_width + self.left_margin,
-                    self.box_position + self.num * (self.box_height+self.box_margin)
-                ),
-                (
-                    (end_pos+1) * self.cell_width + self.left_margin,
-                    self.box_position + self.num * (self.box_height+self.box_margin) + self.box_height
-                )
-            ],
-            fill = self.bar_color,
-            outline = None
-        )
-        self.draw.multiline_text(
-            (
-                start_pos * self.cell_width + self.left_margin + self.font_size/4,
-                self.box_position + self.num * (self.box_height+self.box_margin) + self.font_size/2
-            ),
-            title,
-            fill = self.font_color, 
-            font = ImageFont.truetype(self.font_regular, self.font_size)
-        )
-        self.num = self.num + 1
-
-    def draw_calendar(self):
-
-        week_str = ['月','火','水','木','金','土','日']
-
-        for i in range(self.max_day):
-            
-            d = self.monday+datetime.timedelta(days=i)
-
-            if d.weekday() in [5,6]: #土日は背景を灰色にする
-                self.draw.rectangle(
-                    [
-                        (
-                            self.left_margin + i*self.cell_width,
-                            self.top_margin - self.font_size
-                        ),
-                        (
-                            self.left_margin + (i+1)*self.cell_width,
-                            self.top_margin + self.calendar_height
-                        )
-                    ],
-                    fill = self.holiday_color,
-                    outline = None
-                )
-            else:
-                self.draw.rectangle(
-                    [
-                        (
-                            self.left_margin + i*self.cell_width,
-                            self.top_margin - self.font_size
-                        ),
-                        (
-                            self.left_margin + (i+1)*self.cell_width,
-                            self.top_margin + self.calendar_height
-                        )
-                    ],
-                    fill = self.bg_color,
-                    outline = None
-                )
-
-            self.draw.line(
-                (
-                    self.left_margin + i*self.cell_width, 
-                    self.top_margin,
-                    self.left_margin + i*self.cell_width, 
-                    self.top_margin + self.calendar_height),
-                fill = self.line_color,
-                width = 1
-            )
-
-            if d == self.today:
-                font = ImageFont.truetype(self.font_bold, self.font_size)
-            else:
-                font = ImageFont.truetype(self.font_regular, self.font_size)
-
-            self.draw.multiline_text(
-                (
-                    self.left_margin + i*self.cell_width + self.cell_width/2 - self.font_size/2,
-                    self.date_position
-                ),
-                d.strftime('%d'),
-                fill=self.font_color, 
-                font=font
-            )
-            self.draw.multiline_text(
-                (
-                    self.left_margin + i*self.cell_width + self.cell_width/2 - self.font_size/2,
-                    self.week_position
-                ),
-                week_str[i%7],
-                fill=self.font_color, 
-                font=font
-            )
-
-        i = self.max_day
-        self.draw.line(
-            (
-                self.left_margin + i*self.cell_width, 
-                self.top_margin,
-                self.left_margin + i*self.cell_width, 
-                self.top_margin + self.calendar_height
-            ),
-            fill = (0,0,0),
-            width = 1
-        )
-
-    def show(self):
-        self.im.show()
-
-    def save(self,fname):
-        self.im.save(fname)
-
-    def draw_random_line(self):
-        for x in range(self.im_width):
-            self.draw.line(
-                (
-                    x, 0, x, self.im_height
-                ),
-                fill = (
-                    random.randrange(0,100),
-                    random.randrange(0,100),
-                    random.randrange(0,30)
-                ),
-                width = 1
-            )
+    # Item編集後に呼び出されるcallback
+    def LsitItemActivated(self, evt):
+        row = evt.GetIndex()
+        print(self.GetItem(row, 1).GetText())
+        print(self.GetItem(row, 2).GetText())
+        print(self.GetItem(row, 3).GetText())
 
 
-if __name__ == '__main__':
+class LeftPanel(scrolled.ScrolledPanel):
+    """Task list"""
+    def __init__(self, parent):
+        scrolled.ScrolledPanel.__init__(self, parent=parent)
+        self.SetupScrolling()
 
-    gchart = GanttChart( (720, 320),(255,255,255))
-    gchart.draw_calendar()
-    gchart.draw_campain("2019-10-15","2019-10-18","こんにちは")
-    gchart.draw_campain("2019-10-20","2019-10-23","こんにちは")
-    gchart.draw_campain("2019-10-24","2019-10-30","こんにちは")
-    gchart.draw_campain("2019-10-28","2019-10-30","こんにちは")
-    gchart.draw_campain("2019-10-29","2019-10-30","こんにちは")
-    gchart.show()
-    #gchart.save("test.png")
+        self.list = TaskList(self, 0, style=wx.LC_REPORT|wx.LC_HRULES, size = self.Size)
+
+        button = wx.Button(self, -1, "Create")
+
+        layout = wx.GridSizer(rows=1, cols=1, gap=(0, 0))
+        layout.Add(self.list, flag=wx.EXPAND | wx.TOP,   border=35)
+        self.SetSizer(layout)
+
+
+class RightPanel(scrolled.ScrolledPanel):
+    """Task schedule"""
+    def __init__(self, parent):
+        scrolled.ScrolledPanel.__init__(self, parent=parent)
+        self.SetupScrolling()
+        self.SetBackgroundColour('snow')
+
+        create_calendar.CreateCalendar(self)
+        
+
+class GanttChart(wx.Frame):
+    def __init__(self):
+        wx.Frame.__init__(self, None, title='ChanttChart', size = (1200, 480))
+
+        self.InitMenu()
+ 
+        splitter = wx.SplitterWindow(self, style = wx.SP_LIVE_UPDATE)
+        leftP = LeftPanel(splitter)
+        rightP = RightPanel(splitter)
+        
+        # split the window
+        splitter.SplitVertically(leftP, rightP, 400,)
+        splitter.SetMinimumPaneSize(200)
+        
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(splitter, 1, wx.EXPAND)
+        self.SetSizer(sizer)
+
+    def InitMenu(self):
+        # Menubar
+        menubar = wx.MenuBar()
+        fileMenu = wx.Menu()
+        f_item1 = fileMenu.Append(wx.ID_ANY,'&New(N)', 'Create new task')
+        f_item3 = fileMenu.Append(wx.ID_ANY,'&Quit(Q)', 'Quit application')
+        menubar.Append(fileMenu, '&File(F)')
+        self.SetMenuBar(menubar)
+        # Event call definition
+        self.Bind(wx.EVT_MENU, self.Quit, f_item3)
+        # Frame definition
+        self.Centre()
+        # Status bar
+        self.CreateStatusBar()
+        self.SetStatusText("...")
+
+    def Quit(self, event):
+        self.Close()
+
+
+if __name__ == "__main__":
+    app = wx.App(False)
+    root_frame = GanttChart()
+    root_frame.Show()
+    app.MainLoop()
